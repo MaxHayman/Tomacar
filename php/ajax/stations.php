1e0d986b02948802ac55c388ee826959d70a00a5
Max Hayman <maxhayman@maxhayman.co.uk>
2017-04-17 17:08:52 +0100

    +google maps markers ajax

---
+++ php/ajax/stations.php
@@ -0,0 +1,29 @@
+<?php
+
+$stmt = $conn->query('SELECT
+stations.id,
+stations.`name`,
+stations.capacity,
+stations.lat,
+stations.lng,
+count(stations.id) as count
+FROM
+stations
+LEFT JOIN cars ON stations.id = cars.station
+GROUP BY
+stations.id
+');
+
+$stations = array();
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+	$station = array();
+	$station['name'] = $row['name'];
+	$station['capactity'] = $row['capacity'];
+	$station['lat'] = $row['lat'];
+	$station['lng'] = $row['lng'];
+	$station['count'] = $row['count'];
+	
+	$stations[$row['id']] = $station;
+}
+
+echo json_encode($stations);
